19530fc48aafa7673b7ea7ec1be81a4118f1785a
raduconst <radu.constantin@pixelgrade.com>
2018-08-21 23:04:17 +0300

    added wpforms as a recommended plugin closes #18

--- functions.php
+++ functions.php
@@ -206,4 +206,72 @@ require get_template_directory() . '/inc/hive-hybrid-media-grabber.php';
 /**
  * Theme About page.
  */
-require get_template_directory() . '/inc/admin/about-page.php'; ?>
+require get_template_directory() . '/inc/admin/about-page.php';
+
+require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'hive_lite_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ *
+ * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+ */
+function hive_lite_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		array(
+			'name'      => 'WPForms Lite',
+			'slug'      => 'wpforms-lite',
+			'required'  => false,
+		),
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 */
+	$config = array(
+		'id'           => 'patch-lite',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
+
+/**
+ * Set the WPForms ShareASale ID.
+ *
+ * @param string $shareasale_id The the default ShareASale ID.
+ *
+ * @return string $shareasale_id
+ */
+function hive_lite_wpforms_shareasale_id( $shareasale_id ) {
+
+	// If this WordPress installation already has an WPForms ShareASale ID
+	// specified, use that.
+	if ( ! empty( $shareasale_id ) ) {
+		return $shareasale_id;
+	}
+
+	// Define the ShareASale ID to use.
+	$shareasale_id = '1843354';
+
+	// This WordPress installation doesn't have an ShareASale ID specified, so
+	// set the default ID in the WordPress options and use that.
+	update_option( 'wpforms_shareasale_id', $shareasale_id );
+
+	// Return the ShareASale ID.
+	return $shareasale_id;
+}
+add_filter( 'wpforms_shareasale_id', 'hive_lite_wpforms_shareasale_id' );
+
+?>
