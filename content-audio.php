040fd6e9def38f65e44723601ec01bf0a56987b0
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-07-09 11:17:57 +0300

    added hybrid-media-grabber

--- content-audio.php
+++ content-audio.php
@@ -4,40 +4,45 @@
  * @package Hive Lite 1.1.6
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-
 //get the media objects from the content and bring up only the first one
 $media = hive_audio_attachment(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+		<?php the_title( sprintf( '<a href="%s" class="entry-permalink" rel="bookmark"><h1 class="entry-title">', esc_url( get_permalink() ) ), '</h1></a>' ); ?>
+    </header><!-- .entry-header -->
 
-		<header class="entry-header">
-			<?php the_title( sprintf( '<a href="%s" class="entry-permalink" rel="bookmark"><h1 class="entry-title">', esc_url( get_permalink() ) ), '</h1></a>' ); ?>
-		</header><!-- .entry-header -->
-
-    <? if ( ! empty( $media ) ) : ?>
-		<div class="entry-media">
+	<?php if ( ! empty( $media ) ) : ?>
+        <div class="entry-media">
 			<?php echo $media; ?>
-		</div><!-- .entry-media -->
+        </div><!-- .entry-media -->
 	<?php endif; ?>
 
-	<?php if ( get_the_excerpt() ) { ?>
-		<div class="entry-summary">
-			<?php
+    <div class="entry-summary">
+		<?php
 
-			the_excerpt();
+		the_excerpt();
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hive-lite' ),
-				'after'  => '</div>',
-			) ); ?>
-		</div><!-- .entry-summary -->
-	<?php } ?>
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'hive_txtd' ),
+			'after'  => '</div>',
+		) ); ?>
+    </div><!-- .entry-summary -->
 
-	<?php edit_post_link( esc_html__( 'Edit', 'hive-lite' ), '<div class="edit-link">', '</div>' ); ?>
+	<?php edit_post_link( __( 'Edit', 'hive_txtd' ), '<div class="edit-link">', '</div>' ); ?>
 
-	<?php get_template_part( 'content-footer' ); ?>
+    <footer class="entry-footer">
+        <div class="entry-meta">
+			<?php hivelite_posted_on();
+
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'hive_txtd' ) );
+			if ( $categories_list && hivelite_categorized_blog() ) { ?>
+                <span class="cat-links">
+                        <?php echo $categories_list; ?>
+                    </span>
+			<?php } // End if categories ?>
+        </div><!-- .entry-meta -->
+    </footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
--- functions.php
+++ functions.php
@@ -162,6 +162,11 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/extras.php';
 
 /**
+ * Load the Hybrid Media Grabber class
+ */
+require get_template_directory() . '/inc/hive-hybrid-media-grabber.php';
+
+/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
