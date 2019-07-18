404171d4f5d86c3a7638cdf672e26925d9175117
madalingorbanescu <madalin@pixelgrade.com>
2019-07-18 11:11:29 +0300

    fixed skip link focus close #26

--- functions.php
+++ functions.php
@@ -145,6 +145,8 @@ function hivelite_scripts_styles() {
 	// Enqueue Hive Custom Scripts
 	wp_enqueue_script( 'hive-scripts', get_stylesheet_directory_uri() . '/assets/js/main.js', array( 'jquery', 'masonry', 'hoverIntent', 'velocity' ), $theme->get( 'Version' ), true );
 
+	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
 	if( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
