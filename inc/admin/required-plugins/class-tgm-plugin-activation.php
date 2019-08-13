ca115e8ffde22401614405e53de088e8b032b394
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-08-13 17:39:54 +0300

    Fixed text domain

--- inc/admin/required-plugins/class-tgm-plugin-activation.php
+++ inc/admin/required-plugins/class-tgm-plugin-activation.php
@@ -335,52 +335,52 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 				/* translators: 1: plugin name(s). */
 					'This theme requires the following plugin: %1$s.',
 					'This theme requires the following plugins: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'notice_can_install_recommended'  => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'This theme recommends the following plugin: %1$s.',
 					'This theme recommends the following plugins: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'notice_ask_to_update'            => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
 					'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'notice_ask_to_update_maybe'      => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'There is an update available for: %1$s.',
 					'There are updates available for the following plugins: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'notice_can_activate_required'    => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'The following required plugin is currently inactive: %1$s.',
 					'The following required plugins are currently inactive: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'notice_can_activate_recommended' => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'The following recommended plugin is currently inactive: %1$s.',
 					'The following recommended plugins are currently inactive: %1$s.',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'install_link'                    => _n_noop(
 					'Begin installing plugin',
 					'Begin installing plugins',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'update_link'                     => _n_noop(
 					'Begin updating plugin',
 					'Begin updating plugins',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'activate_link'                   => _n_noop(
 					'Begin activating plugin',
 					'Begin activating plugins',
-					'pixelgrade_assistant'
+					'__theme_txtd'
 				),
 				'return'                          => esc_html__( 'Return to Required Plugins Installer', '__theme_txtd' ),
 				'dashboard'                       => esc_html__( 'Return to the Dashboard', '__theme_txtd' ),
