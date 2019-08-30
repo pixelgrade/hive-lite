904ba0c26de4ca71b560d04461d626849e5ec54a
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-08-30 17:58:03 +0300

    minor

--- inc/integrations.php
+++ inc/integrations.php
@@ -2,11 +2,11 @@
 /**
  * Require files that deal with various plugin integrations.
  *
- * @package Hive
+ * @package Hive Lite
  */
 
 /**
  * Load Customify compatibility file.
  * https://wordpress.org/plugins/customify/
  */
-require get_parent_theme_file_path( '/inc/integrations/customify.php' );
+require_once get_parent_theme_file_path( '/inc/integrations/customify.php' );
